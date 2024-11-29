2686f9f59178721fd2dc093d3afaa59e79612686
PikaChou82 <audrey.saussaye@gmail.com>
2024-11-29 15:28:26 +0100

    Update Mutlimedia_Diffusion.py

--- API/Mutlimedia_Diffusion.py
+++ API/Mutlimedia_Diffusion.py
@@ -9,6 +9,25 @@ from PIL import Image
 # Titre
 st.set_page_config(page_title="Multimedia Diffusion", page_icon=":bar_chart:")
 
+st.markdown("""
+<style>
+button {
+  width: 400px;
+  height: 50px;
+  font-size: 18px;
+  background-color: #4CAF50; /* Vert */
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;   
+
+}
+</style>
+""", unsafe_allow_html=True)
+
 # Créer une mise en page en deux colonnes
 col2, col1 = st.columns([5, 1])
 
@@ -16,6 +35,8 @@ col2, col1 = st.columns([5, 1])
 with col1:
     st.image(f'https://github.com/PikaChou82/Magma_Analytics/blob/main/Images/Multimedia.png?raw=true', width=100)
 
+st.markdown("<h1 style='color:darkblue; font-family:Arial; font-size:50px;'>Bienvenue dans votre espace Multimédia</h1>", unsafe_allow_html=True)
+st.write("")
 st.write("")
 st.write("")
 
@@ -32,6 +53,59 @@ st.markdown(r"""
 </style>
 """, unsafe_allow_html=True)
 
+# Dictionnaire des users
+utilisateur1 = {
+    "id" : "karimadupont",
+    "nom": "Dupont",
+    "prenom": "Karima",
+    "email": "karima.dupont@multimedia-diffusion.com",
+    "preferences": {
+        "type": "films",
+        "langue": "fr",
+        "genre": "action"
+    }
+}
+
+utilisateur2 = {
+        "id" : "pascalejamot",
+       "nom": "Jamot",
+    "prenom": "Pascale",
+    "email": "pascale.jamot@neuf.fr",
+    "preferences": {
+        "type": "films",
+        "langue": "fr",
+        "genre": "comédie"
+    }
+}
+
+utilisateurs = [utilisateur1['id'],utilisateur2['id']]
+
+# Rentrer un nom d'utilisateur pour accéder à sa page personnalisée
+user = st.text_input("Veuillez vous identifier pour plonger dans **votre** monde du cinéma ;)")
+
+# Bouton de validation
+if st.button("Me connecter"):
+    # Si le bouton est cliqué, on affiche un message de confirmation
+    if user in utilisateurs :
+        user = utilisateurs.index(user)
+        user = [utilisateur1, utilisateur2][user]
+        st.write(f"Bonjour {user["prenom"]} ! Bienvenue dans votre espace :)")
+        st.write("")
+        st.write(f"Envie d'un {user['preferences']['type']} style {user["preferences"]["genre"]} ou on change ?")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.button('Go pour mes preferences', on_click=lambda: ouvrir_application('films.py'))
+        with col2:
+            st.button('Envie de changement', on_click=lambda: ouvrir_application('series.py'))
+            
+    else:
+        st.write(f"désolé, je n'ai pas le plaisir de vous connaître... Rdv en cinéma pour vous inscrire avec notre équipe !")
+
+# Détecter le système d'exploitation
+if 'os' not in st.session_state:
+    import platform
+    st.session_state.os = platform.system()
+
 def ouvrir_application(chemin):
     if st.session_state.get('os') == 'Windows':
         subprocess.Popen(f'start {chemin}', shell=True)
@@ -40,10 +114,8 @@ def ouvrir_application(chemin):
     elif st.session_state.get('os') == 'Darwin':  # macOS
         subprocess.Popen(['open', chemin])
 
-st.button('Aller aux films', on_click=lambda: ouvrir_application('films.py'))
-st.button('Aller aux séries', on_click=lambda: ouvrir_application('series.py'))
-
-# Détecter le système d'exploitation (à exécuter une seule fois)
-if 'os' not in st.session_state:
-    import platform
-    st.session_state.os = platform.system()
+col1, col2 = st.columns(2)
+with col1:
+    st.button('Rechercher un film', on_click=lambda: ouvrir_application('films.py'))
+with col2:
+    st.button('Rechercher une série', on_click=lambda: ouvrir_application('series.py'))
