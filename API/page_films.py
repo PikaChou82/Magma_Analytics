d8fd2d9d3f4c8bda0d61dd84aa0836bfee0f2f03
PikaChou82 <audrey.saussaye@gmail.com>
2024-12-05 11:20:02 +0100

    Add files via upload

--- API/page_films.py
+++ API/page_films.py
@@ -1,48 +1,113 @@
-# Import des librairies
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-import time
-from PIL import Image
 
-# Titre
+# Titre et configuration
 st.set_page_config(page_title="Multimedia Diffusion - Vos films", page_icon=":bar_chart:")
 
-# Créer une mise en page en deux colonnes
+# Disposition de la page
 col2, col1 = st.columns([5, 1])
-
-# Afficher le logo dans la première colonne (à droite)
 with col1:
+    # Logo
     st.image(f'https://github.com/PikaChou82/Magma_Analytics/blob/main/Images/Multimedia.png?raw=true', width=100)
-
-st.markdown("<h1 style='color:darkblue; font-family:Arial; font-size:50px;'>Bienvenue dans votre espace films</h1>", unsafe_allow_html=True)
-st.write("")
-st.write("")
+with col2:
+    st.title("Bienvenue dans votre espace films")
 
 # Fond d'écran
 image_url = "https://github.com/PikaChou82/Magma_Analytics/blob/main/Images/fond.jpg?raw=true"
-
-st.markdown(r"""
+st.markdown(f"""
 <style>
-.stApp {
-    background-image: url(""" + image_url + """);
+.stApp {{
+    background-image: url('{image_url}');
     background-size: cover;
     background-position: center center;
-}
+}}
 </style>
 """, unsafe_allow_html=True)
 
-url = "https://raw.githubusercontent.com/PikaChou82/Magma_Analytics/refs/heads/main/Datasets/title_movies_fr_60%2B_85%2B%20(1).csv"
-dataset = pd.read_csv(url, sep =',', encoding='utf-8') 
+
+# Chargement et préparation des données
+url = "https://raw.githubusercontent.com/PikaChou82/Magma_Analytics/refs/heads/main/Datasets/movies_with_reco.csv"
+base_image = "https://image.tmdb.org/t/p/w500/"
+dataset = pd.read_csv(url, sep=',', encoding='utf-8')
 liste_genres = dataset['genres'].unique()
 
-genre = st.selectbox("Choisissez un genre", liste_genres)
-st.write("Vous avez choisi :", genre)
+# Initialisation des variables d'état pour l'affichage des différents blocs
+if 'afficher_bloc_films' not in st.session_state:
+    st.session_state.afficher_bloc_films = True
+if 'film_selectionne' not in st.session_state:
+    st.session_state.film_selectionne = None
+if 'imdb_id' not in st.session_state:
+    st.session_state.imdb_id = None
 
-dataset_filtre = dataset[dataset['genres'] == genre].loc[:,['title','averageRating','startYear']]
-dataset_filtre = dataset_filtre.rename(columns={'title': 'Titre du film', 'averageRating': 'Note moyenne', 'startYear': 'Année de sortie'})
+# Création d'une fonction pour sélectionner un film et forcer les états de blocs
+def select_film(film, imdb_id):
+    st.session_state.film_selectionne = film
+    st.session_state.imdb_id = imdb_id
+    st.session_state.afficher_bloc_films = False
 
-# Afficher le dataframe
-st.dataframe(dataset_filtre, height=300)
+def back():
+    st.session_state.afficher_bloc_films = True
 
+# Bloc 1 : Affichage des films
+if st.session_state.afficher_bloc_films:
+    genre = st.selectbox("Choisissez un genre", liste_genres)
+    st.write("Vous avez choisi :", genre)
+
+    # Filtrer les films selon le genre
+    dataset_filtre = dataset[dataset['genres'] == genre].loc[:, ['title', 'averageRating', 'startYear', 'poster_path', 'imdb_id']]
+    dataset_filtre = dataset_filtre.sort_values(by='averageRating', ascending=False)
+    dataset_filtre = dataset_filtre.rename(columns={'title': 'Titre du film', 'averageRating': 'Note', 'startYear': 'Année', 'poster_path': 'Affiche', 'imdb_id': 'imdb_id'})
+
+    # Affichage des films
+    nb_colonnes = 4
+    cols = st.columns(nb_colonnes)
+    
+    for index, row in dataset_filtre.iterrows():
+        col = cols[index % nb_colonnes]
+        with col:
+            st.markdown(f"**{row['Titre du film']}**")
+            st.write("Année :", row['Année'])
+            st.write("Note :", row['Note'])
+            st.image(base_image + str(row['Affiche']), width=100)
+            
+            # Bouton de sélection
+            st.button(
+                "Voir plus",
+                key=row['imdb_id'],
+                on_click=select_film,
+                args=(row['Titre du film'], row['imdb_id']) 
+            )
+
+# Bloc 2 : Détails du film sélectionné
+
+if not st.session_state.afficher_bloc_films and st.session_state.film_selectionne:
+
+    st.button("Back to Films",on_click=back)
+
+    st.write(f"### {st.session_state.film_selectionne}")
+    row_film = dataset[dataset['imdb_id'] ==st.session_state.imdb_id].index[0]
+    voisins = pd.DataFrame(dataset.loc[row_film, ['Voisin1', 'Voisin2', 'Voisin3', 'Voisin4']])
+    propal = voisins.iloc[:,0].to_list()
+    try : propal.remove(row_film)
+    except : propal.pop()
+    st.image(base_image + str(dataset.loc[row_film,'poster_path']), width=200)
+    st.write("")
+    st.write(f"**Année** : {dataset.loc[row_film,'startYear']} - **Recommandé à** {dataset.loc[row_film,'averageRating']*10} %")
+    st.write(f"**Scenario**\n{dataset.loc[row_film,'overview']}")
+    st.write("")
+    st.write("Vous pourriez aussi aimer :")
+    colonnes = st.columns(3)
+    for i in range(len(propal)):
+        with colonnes[i]:
+            st.markdown(f"**{dataset.loc[propal[i],'title']}**")
+            st.write("Année :", dataset.loc[propal[i],'startYear'])
+            st.write("Note :", dataset.loc[propal[i],'averageRating'])
+            st.image(base_image + str(dataset.loc[propal[i],'poster_path']), width=100)
+            
+            # Bouton de sélection
+            st.button(
+                "Voir plus",
+                key=dataset.loc[propal[i],'imdb_id'],
+                on_click=select_film,
+                args=(dataset.loc[propal[i],'title'], dataset.loc[propal[i],'imdb_id']) 
+            )
